d279429571e095d39309645d195637a0e40af259
wolfenmor <wolfenmor@gmail.com>
2023-11-21 01:58:20 +0100

    JS; lesson1

---
+++ JavaScript/lesson1/main.js
@@ -0,0 +1,28 @@
+let intro = "hello"
+let str1 = "owu"
+let str2 = "com"
+let str3 = "ua"
+console.log(`${intro} ${str1}` + "@" + str2 + "." + str3)
+
+let n = 1
+let n1 = 10
+let n2 = -999
+let n3 = 123
+let n4 = 2.7
+let n5 = 16
+let pi = 3.14
+console.log(n, n1, n2, n3, pi, n4, n5)
+
+let firstName = "Max"
+let middleName = "Khymchyk"
+let lastName = "yuriovuch"
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+prompt("")
