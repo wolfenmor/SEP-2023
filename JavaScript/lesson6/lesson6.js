eae5037bfdc2885048e6c81635cc0af4a2bc774b
wolfenmor <wolfenmor@gmail.com>
2023-12-08 01:48:51 +0100

    lesson6.js

--- JavaScript/lesson6/lesson6.js
+++ JavaScript/lesson6/lesson6.js
@@ -66,17 +66,34 @@ else if (direction === "descending" ){
 
 // ==========================
 // - є масив
-// let coursesAndDurationArray = [
-//   {title: 'JavaScript Complex', monthDuration: 5},
-//   {title: 'Java Complex', monthDuration: 6},
-//   {title: 'Python Complex', monthDuration: 6},
-//   {title: 'QA Complex', monthDuration: 4},
-//   {title: 'FullStack', monthDuration: 7},
-//   {title: 'Frontend', monthDuration: 4}
-// ];
+ let coursesAndDurationArray = [
+   {title: 'JavaScript Complex', monthDuration: 5},
+   {title: 'Java Complex', monthDuration: 6},
+   {title: 'Python Complex', monthDuration: 6},
+   {title: 'QA Complex', monthDuration: 4},
+   {title: 'FullStack', monthDuration: 7},
+   {title: 'Frontend', monthDuration: 4}
+ ];
 // -- відсортувати його за спаданням за monthDuration
+let res = coursesAndDurationArray.sort((a,b) =>{
+
+  return b.monthDuration - a.monthDuration
+});
+console.log(res)
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+coursesAndDurationArray.filter(function (item){
+  if (item.monthDuration > 5){
+    console.log(item)
+  }
+})
+
 // -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+let newCoursesArray = coursesAndDurationArray.map((course, index) =>{
+  course.id = index+1
+  return course
+})
+
+console.log(newCoursesArray)
 // =========================
 //   описати колоду карт (від 6 до туза без джокерів)
 // - знайти піковий туз
@@ -91,6 +108,44 @@ else if (direction === "descending" ){
 //   color:'', // 'red','black'
 // }
 //
+deck = [
+  { cardSuit: "spade", value: '6', color: "black" },
+  { cardSuit: "spade", value: '7', color: "black" },
+  { cardSuit: "spade", value: '8', color: "black" },
+  { cardSuit: "spade", value: '9', color: "black" },
+  { cardSuit: "spade", value: '10', color: "black" },
+  { cardSuit: "spade", value: 'jack', color: "black" },
+  { cardSuit: "spade", value: 'queen', color: "black" },
+  { cardSuit: "spade", value: 'king', color: "black" },
+  { cardSuit: "spade", value: 'ace', color: "black" },
+  { cardSuit: "diamond", value: '6', color: 'red' },
+  { cardSuit: "diamond", value: '7', color: 'red' },
+  { cardSuit: "diamond", value: '8', color: 'red' },
+  { cardSuit: "diamond", value: '9', color: 'red' },
+  { cardSuit: "diamond", value: '10', color: 'red' },
+  { cardSuit: "diamond", value: 'jack', color: 'red' },
+  { cardSuit: "diamond", value: 'queen', color: 'red' },
+  { cardSuit: "diamond", value: 'king', color: 'red' },
+  { cardSuit: "diamond", value: 'ace', color: 'red' },
+  { cardSuit: "heart", value: '6', color: 'red' },
+  { cardSuit: "heart", value: '7', color: 'red' },
+  { cardSuit: "heart", value: '8', color: 'red' },
+  { cardSuit: "heart", value: '9', color: 'red' },
+  { cardSuit: "heart", value: '10', color: 'red' },
+  { cardSuit: "heart", value: 'jack', color: 'red' },
+  { cardSuit: "heart", value: 'queen', color: 'red' },
+  { cardSuit: "heart", value: 'king', color: 'red' },
+  { cardSuit: "heart", value: 'ace', color: 'red' },
+  { cardSuit: "club", value: '6', color: 'black' },
+  { cardSuit: "club", value: '7', color: 'black' },
+  { cardSuit: "club", value: '8', color: 'black' },
+  { cardSuit: "club", value: '9', color: 'black' },
+  { cardSuit: "club", value: '10', color: 'black' },
+  { cardSuit: "club", value: 'jack', color: 'black' },
+  { cardSuit: "club", value: 'queen', color: 'black' },
+  { cardSuit: "club", value: 'king', color: 'black' },
+  { cardSuit: "club", value: 'ace', color: 'black' },
+]
 // =========================
 //
 //   Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
@@ -100,7 +155,101 @@ else if (direction === "descending" ){
 //   hearts:[],
 //   clubs:[]
 // }
+console.log(deck.reduce((accumulator, item) => {
+  if (item.cardSuit === "spade"){
+    accumulator.spades.push(item)
+  }
+  else if (item.cardSuit === "diamond"){
+    accumulator.diamonds.push(item)
+  }
+  else if (item.cardSuit === "heart"){
+    accumulator.hearts.push(item)
+  }
+  else if (item.cardSuit === "club"){
+    accumulator.clubs.push(item)
+  }
+  return accumulator;
+
+}, {spades: [], diamonds: [], hearts: [], clubs: []}))
+
 // =========================
 //   взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+let coursesArray = [
+  {
+    title: 'JavaScript Complex',
+    monthDuration: 5,
+    hourDuration: 909,
+    modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+  },
+  {
+    title: 'Java Complex',
+    monthDuration: 6,
+    hourDuration: 909,
+    modules: ['html',
+      'css',
+      'js',
+      'mysql',
+      'mongodb',
+      'angular',
+      'aws',
+      'docker',
+      'git',
+      'java core',
+      'java advanced']
+  },
+  {
+    title: 'Python Complex',
+    monthDuration: 6,
+    hourDuration: 909,
+    modules: ['html',
+      'css',
+      'js',
+      'mysql',
+      'mongodb',
+      'angular',
+      'aws',
+      'docker',
+      'python core',
+      'python advanced']
+  },
+  {
+    title: 'QA Complex',
+    monthDuration: 4,
+    hourDuration: 909,
+    modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+  },
+  {
+    title: 'FullStack',
+    monthDuration: 7,
+    hourDuration: 909,
+    modules: ['html',
+      'css',
+      'js',
+      'mysql',
+      'mongodb',
+      'react',
+      'angular',
+      'aws',
+      'docker',
+      'git',
+      'node.js',
+      'python',
+      'java']
+  },
+  {
+    title: 'Frontend',
+    monthDuration: 4,
+    hourDuration: 909,
+    modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+  }
+];
 // --написати пошук всіх об'єктів, в який в modules є sass
+let result = coursesArray.filter(value =>
+  value.modules.includes("sass")
+)
+console.log(result)
 // --написати пошук всіх об'єктів, в який в modules є docker
+let result1 = coursesArray.filter(value =>
+  value.modules.includes("docker")
+)
+console.log(result1)
